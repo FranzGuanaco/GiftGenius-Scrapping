0ddac27e50b537fdaff21a144571470fabd69f3d
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2024-02-03 15:30:46 +0100

    scrapping script done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const { Client } = require('pg'); // PostgreSQL client
 const axios = require('axios');
+const { error } = require('console');
 
 // Fonction principale asynchrone
 async function main() {
@@ -62,37 +63,51 @@ async function main() {
     const response = await axios.get(imageURL, { responseType: 'arraybuffer' });
     const imageBuffer = Buffer.from(response.data);
 
-    // Insertion de name dans Seller
-    const SellerName = 'INSERT INTO seller (name) VALUES ($1) RETURNING pk';
-    const SellerValue = ['Le new test'];
+    // Insertion de name dans Seller s'il n'existe pas encore
+    const SellerName = 'INSERT INTO seller (name) VALUES ($1) ON CONFLICT (name) DO NOTHING RETURNING pk';
+    const SellerValue = ['Le premier ecom'];
 
     // Utilisez SellerId comme clé étrangère de SellerValue
     const sellerResult = await client.query(SellerName, SellerValue);
 
     // Verification et de la creation du sellerID pour l'inserer dans products comme FK
    
-      const sellerId = sellerResult.rows[0].pk;
-      console.log('sellerId récupéré:', sellerId); // Ceci devrait afficher le sellerId récupéré
+    let sellerId;
+    let result;
+
+    // Verification que l'acteur seller a bien été créé
+    if (sellerResult.rows.length > 0) {
+        sellerId = sellerResult.rows[0].pk;
+        console.log('sellerId récupéré:', sellerId);
+    } else {
+        // Si aucune ligne n'est retournée, cela signifie que le vendeur existe déjà.
+        // Vous devez alors récupérer son ID manuellement.
+        const fetchSellerId = 'SELECT pk FROM seller WHERE name = $1';
+        // result permet de selectionner le row dans la table seller ou le nom est celui deja mentionné dans le cas ou il existe deja
+        result = await client.query(fetchSellerId, SellerValue);
+        sellerId = result.rows[0].pk;
+        console.log('sellerId existant récupéré:', sellerId);
+    }
     
+   
       // Utilisez sellerId pour l'insertion dans products
-      const productInsertQuery = 'INSERT INTO products (name, description, seller_id, photo) VALUES ($1, $2, $3, $4) RETURNING pk;'
+      const productInsertQuery = 
+      'INSERT INTO products (name, description, seller_id, photo) VALUES ($1, $2, $3, $4) ON CONFLICT (name, seller_id) DO UPDATE SET description = EXCLUDED.description, photo = EXCLUDED.photo RETURNING pk;'
       const productValues = [productNameText, descriptionText, sellerId, imageBuffer];
       const productResult = await client.query(productInsertQuery, productValues);
-      const productId = productResult.rows[0].pk;
-    
-      // Autres opérations...
+// Si le produit existe la row est superieur et on accede a la premiere row existante à 0 sinon cela veut dire que cela n'existe pas et que c'est null
+      const productId = productResult.rows.length > 0 ? productResult.rows[0].pk : null;
    
 
     try {
-      if (sellerResult.rows.length >= 0 )  {
+      if (sellerResult.rows.length > 0 )  {
         // Assurez-vous que l'indice [2] est correct en fonction de la structure des résultats
         const sellerId = sellerResult.rows[0].pk;
-        const productId = productResult.rows[0].pk;
 
         console.log('Voici selledID:', sellerId);
 
 
-        // Insertion de price dans Price
+        // Insertion de price et product comme fk dans Price
         const priceInsertQuery = 'INSERT INTO price (seller, price, link, product) VALUES ($1, $2, $3, $4)';
         const priceValues = [sellerId, priceText, imageURL, productId];
 
@@ -102,26 +117,36 @@ async function main() {
 
       
       } else {
-        console.error('La requête pour Seller n\'a renvoyé aucune ligne');
+        const sellerId = result.rows[0].pk;
+    
+        console.log('Voici sellerID:', sellerId);
+    
+        // Mise à jour de price dans la table Price
+        const priceUpdateQuery = 'UPDATE price SET seller = $1, price = $2, link = $3 WHERE product = $4';
+        const priceValues = [sellerId, priceText, imageURL, productId];
+        await client.query(priceUpdateQuery, priceValues);
+
+        console.error('La requête à updater la colonne Seller');
         // Gérez le cas où la requête pour Seller ne renvoie aucune ligne
       }
     } catch (sellerError) {
       console.error('Une erreur s\'est produite lors de l\'insertion des données Seller:', sellerError);
     }
+
+    // async pour l'insertion de données et de la FK dans Occasion 
     
     try {
       if (productResult.rows.length > 0) {
-  
-        const productId = productResult.rows[0].pk;
 
         // Insertion dans Occasion de la FK
-        const OccasionInsert = 'INSERT INTO occasion (product) VALUES ($1)';
+        const OccasionInsert = 'INSERT INTO occasion (product) VALUES ($1) ON CONFLICT (product) DO UPDATE SET product = $1';
         const Occasion = [productId];
 
         await client.query(OccasionInsert, Occasion);
 
       } else {
-        console.error('La requête pour Product n\'a renvoyé aucune ligne');
+
+        console.error('Erreur product result est inferieur à 0');
         // Gérez le cas où la requête pour Product ne renvoie aucune ligne
       }
     } catch (productError) {
